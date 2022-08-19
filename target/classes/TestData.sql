97f36916c8b7998741503c120bf60f6cebbf7cf4
halim <halim261@gmail.com>
2022-08-19 20:08:23 +0800

    preperation for deployment

--- target/classes/TestData.sql
+++ target/classes/TestData.sql
@@ -1,10 +1,10 @@
-INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Halim", "halim", "halim@gmail.com", "smut");
-INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Heily", "heily", "heily@hotmail.com", "Engineer");
+INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Halim", "halim", "halim@gmail.com", "smut"), ("Heily", "heily", "heily@hotmail.com", "Engineer");
 INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Xingjie", "xingjie", "xingjie@gmail.com", "NEET");
 INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Yeemon", "yeemon", "yeemon@yahoo.sg", "Student");
 INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Nyein", "nyein", "bellanyein@gmail.com", "Engineer");
+INSERT INTO ADproj.user(name, password, email, occupation) VALUES ("Nina", "nina", "nina@gmail.com", "Scientist");
 
-INSERT INTO ADproj.activity (date_time, description, points, trash_type, user_id) VALUES('2022-08-07', 'Added Picture', 1, 'PLASTIC', 1);
+INSERT INTO ADproj.activity (date_time, description, points, trash_type, user_id) VALUES('2022-08-07', 'Added Picture', 1, 'PLASTIC', 1),
 INSERT INTO ADproj.activity (date_time, description, points, trash_type, user_id) VALUES('2022-08-08', 'Added Picture', 1, 'METAL', 2);
 INSERT INTO ADproj.activity (date_time, description, points, trash_type, user_id) VALUES('2022-08-07', 'Added Picture', 1, 'GLASS', 1);
 INSERT INTO ADproj.activity (date_time, description, points, trash_type, user_id) VALUES('2022-08-08', 'Added Picture', 1, 'PLASTIC', 3);
